d037ec5852df398e13a15f25fca3f2a43cef008e
JMJones716 <J.M.Jones3@wlv.ac.uk>
2025-05-07 05:37:54 +0100

    Fixed minor issue with unescaped apostrophe. This was preventing netlify from building the site due to ESling rules.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
     return (
       <main style={{ textAlign: "center", padding: "4rem" }}>
         <h1>404 - Page Not Found</h1>
-        <p>Sorry, we couldn't find the page you were looking for.</p>
+        <p>Sorry, we couldn&apos;t find the page you were looking for.</p>
       </main>
     );
   }
